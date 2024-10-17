e78c1e00af0e05111592fb61710cff45168b648a
Artem <demchenkoartem066@gmail.com>
2024-10-17 12:18:27 +0300

    Перший коміт

---
+++ src/pages/scss/index.js
@@ -0,0 +1,6 @@
+// Імпортуємо стилі
+import './normalize.css';  // Шлях до normalize.css
+import './styles.scss';    // Шлях до основних стилів
+
+// Повідомлення в консоль
+console.log('Hello, Webpack!');
